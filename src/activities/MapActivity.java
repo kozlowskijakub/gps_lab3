0c968cf508524d793190827b87bf7ec54cf1a17c
kozlowskijakub <kooba007@gmail.com>
2013-10-29 18:45:58 +0100

    Made drawing, insert MyLocation View in code (Activity onCreate) changed from .xml

--- src/activities/MapActivity.java
+++ src/activities/MapActivity.java
@@ -1,15 +1,14 @@
 package activities;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Paint;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.example.gps_lab3.R;
+import map.MyLocation;
 import map.POI;
 
 import java.util.ArrayList;
@@ -20,34 +19,25 @@ public class MapActivity extends Activity implements LocationListener {
     private LocationManager myLocationManager;
     private LocationListener myLocationLister;
     private POI poi;
-    public ArrayList<POI> poiList = new ArrayList<POI>();
-    Paint paint = new Paint();
+    public static ArrayList<POI> poiList = new ArrayList<POI>();
+    TextView lattitude;
+    TextView longitude;
+    MyLocation ml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        try {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.mapview);
-            myLocationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
-            myLocationLister = this;
-            myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationLister);
-        } catch (Exception e) {
-            Log.e("myException", e.toString());
-//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapview);
+        myLocationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        myLocationLister = this;
+        myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationLister);
 
-    }
-
-    public void drawGps(View v) {
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void showMap(View view) {
-        Intent intent = new Intent(this, MapActivity.class);
-        startActivity(intent);
+        this.lattitude = (TextView) findViewById(R.id.tv_lattitude);
+        this.longitude = (TextView) findViewById(R.id.tv_longitude);
+        ml = new MyLocation(this);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.ll_mapView);
+        ll.addView(ml, ll.getChildCount());
     }
 
 
@@ -56,35 +46,25 @@ public class MapActivity extends Activity implements LocationListener {
         this.poi = new POI();
         this.poi.xCoordinate = location.getLatitude();
         this.poi.yCoordinate = location.getLongitude();
-        this.poiList.add(poi);
+        poiList.add(poi);
 
+        this.lattitude.setText(String.valueOf(poi.yCoordinate));
+        this.longitude.setText(String.valueOf(poi.xCoordinate));
+        ml.invalidate();
     }
 
-//
-//    public void drawGps(View v) {
-//        GPSStatusView gpsStatusView = new GPSStatusView(this);
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-//                LinearLayout.LayoutParams.MATCH_PARENT);
-//        LinearLayout ll = (LinearLayout) findViewById(R.id.ll_main);
-//
-//        if (draw == false)
-//            ll.removeViewAt(ll.getChildCount() - 1);
-//        this.draw = false;
-//        ll.addView(gpsStatusView, params);
-//    }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        //To change body of implemented methods use File | Settings | File Templates.
+
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        //To change body of implemented methods use File | Settings | File Templates.
+
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
--- src/map/MyLocation.java
+++ src/map/MyLocation.java
@@ -1,67 +1,62 @@
 package map;
 
+import activities.MapActivity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-
-import java.util.ArrayList;
 
 
 public class MyLocation extends View {
-    private POI poi;
-    public ArrayList<POI> poiList = new ArrayList<POI>();
+
+    //    public ArrayList<POI> poiList = new ArrayList<POI>();
     Paint paint = new Paint();
+
 
     public MyLocation(Context context) {
         super(context);
+//        this.setId(10);
     }
 
     public MyLocation(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    public MyLocation(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    public MyLocation(Context context, POI poi, ArrayList<POI> poiList, Paint paint) {
-        super(context);
-        this.poi = poi;
-        this.poiList = poiList;
-        this.paint = paint;
-    }
-
-    public MyLocation(Context context, AttributeSet attrs, POI poi, ArrayList<POI> poiList, Paint paint) {
-        super(context, attrs);
-        this.poi = poi;
-        this.poiList = poiList;
-        this.paint = paint;
-    }
-
-    public MyLocation(Context context, AttributeSet attrs, int defStyle, POI poi, ArrayList<POI> poiList, Paint paint) {
-        super(context, attrs, defStyle);
-        this.poi = poi;
-        this.poiList = poiList;
-        this.paint = paint;
+//        this.setId(10);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 //        super.onDraw(canvas);    //To change body of overridden methods use File | Settings | File Templates.
-
+        setBackgroundColor(Color.YELLOW);
         double xCenter = this.getWidth();
         double YCenter = this.getWidth();
 
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(100, 100, 100, paint);
+//        canvas.drawCircle(100, 100, 100, paint);
+//        paint.setStyle(Paint.Style.FILL);
+        paint.setStyle(Paint.Style.STROKE);
+        float OFFSET = 50;
+        for (POI poi : MapActivity.poiList) {
+            canvas.drawPoint((float) (poi.xCoordinate - 49.84f) * 5000 + 6 * OFFSET, (float) (poi.yCoordinate - 19f) * 5000 + OFFSET, paint);
 
-//        for (POI poi : poiList) {
-//            canvas.drawPoint((float) poi.xCoordinate, (float) poi.yCoordinate, paint);
-//        }
-//        this.invalidate();
+        }
+
+        try {
+            canvas.drawText((String.valueOf((MapActivity.poiList.get(MapActivity.poiList.size() - 1).xCoordinate - 49.84f) * 2500)), 100, 100, paint);
+            canvas.drawText((String.valueOf((MapActivity.poiList.get(MapActivity.poiList.size() - 1).yCoordinate - 19f) * 2500)), 100, 150, paint);
+        } catch (Exception e) {
+            Log.e("myException", e.toString());
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+
+    }
+
+
+    public void repaint() {
+        this.invalidate();
     }
 }
